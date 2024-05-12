343f651c2e0c2be0306e4a6b01df83cda9ea2d61
Daniel Navarrete <11926357+Danieliv@users.noreply.github.com>
2024-05-12 14:39:33 -0700

    Set databaseUrl constant with database path and used to connect to RestDatabase database

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+const databaseUrl = "mongodb://localhost:27017/RestDatabase";
+
+mongoose.connect(databaseUrl, { useNewUrlPaserr: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
+
 app.listen(port, () => {
   console.log(`Server has started in port ${port}`);
 });
