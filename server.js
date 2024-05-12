2ebafbc46b07f34a7d291cf6ed31c0bed3921624
Daniel Navarrete <11926357+Danieliv@users.noreply.github.com>
2024-05-12 00:24:00 -0700

    Configured app to use body-parser to get json data

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(`Server has started in port ${port}`);
 });
