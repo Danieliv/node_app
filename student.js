abb1b10bdc939e4d4abc175a59dab2115066833a
Daniel Navarrete <11926357+Danieliv@users.noreply.github.com>
2024-05-16 02:21:02 -0700

    Updated numberOfCourses property type from Integer to Number

--- student.js
+++ student.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
 const StudentSchema = new Schema({
 	name: String,
-	numberOfCourses: Integer
+	numberOfCourses: Number
 });
 
 module.exports = mongoose.model("Student", StudentSchema);  
